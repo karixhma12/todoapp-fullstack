f2617f225900a534d7e10bbc38c6475f92dd0beb
Karishma <karishma.7022@gmail.com>
2026-06-04 08:20:13 +0530

    step 8: signup page complete

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,26 @@
-
+import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function Signup(){
+
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+    const navigate = useNavigate();
+
+    async function submitform(){
+        await axios.post("http://localhost:3000/api/auth/signup",{username,email,password})
+        console.log("form submitted to backend");
+        navigate("/login");
+    }
+
     return (
         <div>
-            This is a Signup element
+            <input type="text" placeholder="enter your username" value={username} onChange={(e)=>{setUsername(e.target.value)}} ></input>
+            <input type="text" placeholder="enter your email-id" value={email} onChange={(e)=>{setEmail(e.target.value)}} ></input>
+            <input type="text" placeholder="enter your password" value={password} onChange={(e)=>{setPassword(e.target.value)}} ></input>
+            <button onClick={()=>{submitform()}}> Submit </button>
         </div>
     )
 }
