15578040ffe852119c4d6f0802591a7784a4e8b1
Karishma <karishma.7022@gmail.com>
2026-06-04 08:33:54 +0530

    step 9: login page complete with JWT stored in localStorage:

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,26 @@
-
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import {useState} from "react";
 
 function Login(){
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+
+    async function submitform(){
+        const response = await axios.post("http://localhost:3000/api/auth/login",{email,password});
+        console.log("login form submitted!");
+        localStorage.setItem("token",response.data.token);
+        navigate("/todos");
+        
+    }
+
     return(
         <div>
-            This is a login element
+            <input type="text" placeholder="enter your email" value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <input type="text" placeholder="enter your password" value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button onClick={()=>submitform()}> Submit </button>
         </div>
     )
 }
