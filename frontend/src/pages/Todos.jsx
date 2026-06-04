de570cf897ed143b5a8390c32b724918a3ba7c4a
Karishma <karishma.7022@gmail.com>
2026-06-04 11:51:40 +0530

    step 11: protected routing for todos page

--- frontend/src/pages/Todos.jsx
+++ frontend/src/pages/Todos.jsx
@@ -1,11 +1,20 @@
 import {useState,useEffect} from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function Todos(){
 
     const [todo,setTodo] = useState("");
     const [todos,setTodos] = useState([]);
     const [refresh,setRefresh] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(!token){
+            navigate("/login");
+        }
+    },[]);
 
     useEffect(()=>{
         async function fetchTodos(){
